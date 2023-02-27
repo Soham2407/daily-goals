d8c3bef295c086938838a6387ccc60105135d04a
Soham2407 <nesarikarsoham24@gmail.com>
2023-02-27 08:53:11 +0530

    Daily Goals completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Container } from "@mui/material";
+import Header from "./components/Header";
 import Content from "./components/Content";
 import Footer from "./components/Footer";
-import Header from "./components/Header";
+
+const getItemFromLocalStorage = () => {
+  const items = JSON.parse(localStorage.getItem("goals"));
+  if (items) {
+    return items;
+  } else {
+    return [];
+  }
+};
 
 function App() {
-  const [goals, setGoals] = useState([]);
+  const [goals, setGoals] = useState(getItemFromLocalStorage());
   const [searchTerm, setSearchTerm] = useState("");
-  const [filterGoals, setFilterGoals] = useState([]);
+  const [filterGoals, setFilterGoals] = useState(goals);
+
+  useEffect(() => {
+    localStorage.setItem("goals", JSON.stringify(goals));
+  }, [goals]);
+
+  useEffect(() => {
+    const filterItems = goals.filter((goal) =>
+      goal.title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setFilterGoals(filterItems);
+  }, [searchTerm, goals]);
 
   const handleSearch = (e) => {
     setSearchTerm(e.target.value);
-    if (searchTerm !== "") {
-      const updateGoals = goals.filter((goal) =>
-        goal.title.toLowerCase().includes(searchTerm.toLowerCase())
-      );
-      setFilterGoals(updateGoals);
-    } else {
-      setFilterGoals(goals);
-    }
   };
   return (
     <Container className="app">
@@ -27,10 +39,7 @@ function App() {
         handleSearch={handleSearch}
         searchTerm={searchTerm}
       />
-      <Content
-        goals={searchTerm.length < 1 ? goals : filterGoals}
-        setGoals={setGoals}
-      />
+      <Content goals={filterGoals} setGoals={setGoals} />
       <Footer />
     </Container>
   );
